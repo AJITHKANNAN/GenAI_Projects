105e8355a854cd2354aa2c08c253496fecb88de9
Ajith <ajithkannan13698@gmail.com>
2025-10-14 18:21:28 +0530

    Update app.py

--- ChatPDF/app.py
+++ ChatPDF/app.py
@@ -11,8 +11,6 @@ from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
 from langchain.embeddings import HuggingFaceEmbeddings
 
-
-
 load_dotenv()
 os.getenv("GOOGLE_API_KEY")
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
